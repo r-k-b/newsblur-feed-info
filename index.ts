6a5a0ee77abfb6ff6453b27b3f547cf5b3e4dd1b
Robert K. Bell <robert.k.bell@gmail.com>
2016-06-04 20:09:16 +1000

    test: add more

--- index.ts
+++ index.ts
@@ -1,11 +1,59 @@
 /// <reference path="typings/index.d.ts" />
 
-import 'fs';
+import * as fs from 'fs';
+import {merge, prop, curry} from 'ramda';
+import * as Path from 'path';
 import {Observable} from 'rxjs';
 import {parseString} from 'xml2js';
+import * as stringifyOld from 'json-stable-stringify';
 
-const readFileStream = (path: string) => Observable.of('x');
+interface StringifyOptions {
+    space?: number
+}
+
+
+const stringify = (options:StringifyOptions = {}) => (obj: any) => stringifyOld(obj, options);
+
+
+const getFilenameMetaData = (path:string) => ({
+    extension: Path.extname(path),
+    location: Path.dirname(path),
+    name: Path.basename(path, Path.extname(path)),
+    path: path
+});
+
+/**
+ *   path → Observable( file )
+ */
+const readFileStream = (path:string = '', encoding:string = 'utf-8') =>
+    Observable.create(function (observer) {
+        function readFileStreamCB(e, file) {
+            if (e) return observer.error(e);
+
+            observer.next(merge(
+                getFilenameMetaData(path || ''),
+                {file}
+            ));
+            observer.complete();
+        }
+
+        fs.readFile(path, encoding, readFileStreamCB);
+    });
+
+
+const xmlToObservable = Observable.bindNodeCallback(parseString);
+
+// readFileStream('opml/webcomics.xml')
+//     .map(prop('file'))
+//     // .do(x => console.log({'rfs result':x}))
+//     .flatMap(xmlToObservable)
+//     .map(stringify({}))
+//     .subscribe(x => {console.log({x})});
+
+
 
 export {
-    readFileStream
+    readFileStream,
+    xmlToObservable,
+    stringify
 }
--- test/index-spec.ts
+++ test/index-spec.ts
@@ -13,3 +13,41 @@ test('readfile and rxjs', t => {
 
     t.end();
 });
+
+
+test('map a xml file into an object', t => {
+    t.plan(1);
+
+    index.xmlToObservable('<foo>bar</foo>').subscribe(x => {
+        t.deepEqual(x, {foo: 'bar'}, 'simple xml');
+    });
+});
+
+
+test('better stringify', t => {
+    // t.equal(
+    //     index.stringify({}, {foo:'bar'}),
+    //     '{"foo":"bar"}',
+    //     'uncurried & no options works'
+    // );
+
+    // t.equal(
+    //     index.stringify({space: 2}, {foo:'bar'}),
+    //     '{\n  "foo": "bar"\n}`,
+    //     'uncurried & "space" option works'
+    // );
+
+    t.equal(
+        index.stringify({})({foo:'bar'}),
+        '{"foo":"bar"}',
+        'curried & no options works'
+    );
+
+    t.equal(
+        index.stringify({})({foo:'bar'}),
+        '{"foo":"bar"}',
+        'curried & no options works'
+    );
+
+    t.end();
+});
